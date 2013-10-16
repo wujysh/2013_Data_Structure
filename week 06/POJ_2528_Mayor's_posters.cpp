af134d877014ceccbf404c6bf88edbf13a5d8777
Wujysh <wujysh@gmail.com>
2013-10-16 16:09:53 +0800

    Update POJ_2528_Mayor's_posters.cpp

--- week 06/POJ_2528_Mayor's_posters.cpp
+++ week 06/POJ_2528_Mayor's_posters.cpp
@@ -13,7 +13,7 @@ int cnt;
 
 void PushDown(int rt) {
 	if (col[rt] != -1) {
-		col[rt<<1] = col[(rt<<1)|1] = col[rt];
+		col[rt << 1] = col[(rt << 1) | 1] = col[rt];
 		col[rt] = -1;
 	}
 }
@@ -26,10 +26,10 @@ void update(int L, int R, int c, int l, int r, int rt) {
 	PushDown(rt);
 	int m = (l + r) >> 1;
 	if (L <= m) update(L, R, c, l, m, rt << 1);
-	if (m < R) update(L, R, c, m+1, r, (rt << 1) | 1);
+	if (m < R) update(L, R, c, m + 1, r, (rt << 1) | 1);
 }
 
-void query(int l,int r,int rt) {
+void query(int l, int r, int rt) {
 	if (col[rt] != -1) {
 		if (!hashi[col[rt]]) cnt++;
 		hashi[col[rt]] = true;
@@ -39,7 +39,7 @@ void query(int l,int r,int rt) {
 
 	int m = (l + r) >> 1;
 	query(l, m, rt << 1);
-	query(m+1, r, (rt << 1) | 1);
+	query(m + 1, r, (rt << 1) | 1);
 }
 
 int Bin(int key, int n, int X[]) {
@@ -56,10 +56,10 @@ int Bin(int key, int n, int X[]) {
 int main() {
 	int T, n;
 	scanf("%d", &T);
-	while (T --) {
+	while (T--) {
 		scanf("%d", &n);
 		int nn = 0;
-		for (int i = 0; i < n; i ++) {
+		for (int i = 0; i < n; i++) {
 			scanf("%d%d", &li[i], &ri[i]);
 			X[nn++] = li[i];
 			X[nn++] = ri[i];
@@ -67,16 +67,16 @@ int main() {
 
 		sort(X, X + nn);
 		int m = 1;
-		for (int i = 1; i < nn; i ++) {
+		for (int i = 1; i < nn; i++) {
 			if (X[i] != X[i-1]) X[m++] = X[i];
 		}
 		for (int i = m - 1; i > 0; i--) {
-			if (X[i] != X[i-1] + 1) X[m ++] = X[i-1] + 1;
+			if (X[i] != X[i-1] + 1) X[m++] = X[i-1] + 1;
 		}
 		sort(X, X + m);
 
 		memset(col, -1, sizeof(col));
-		for (int i = 0; i < n; i ++) {
+		for (int i = 0; i < n; i++) {
 			int l = Bin(li[i], m, X);
 			int r = Bin(ri[i], m, X);
 			update(l, r, i, 0, m, 1);
